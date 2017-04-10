e70f400ede15bf6ea7025a255884b897eeca5f97
Danny DeRuiter <dannydf>
2017-04-09 23:41:37 -0400

    Walls are generating

---
+++ js/models/Walls.js
@@ -0,0 +1,43 @@
+/**
+ * Created by dannyd1221 on 4/9/2017.
+ */
+define(function(require){
+    var THREE = require('../vendor/three');
+    class Walls{
+        constructor(){
+            const WALL_HEIGHT = 200;
+            const WALL_WIDTH = 200;
+            const WALL_DEPTH = 1000;
+            const cobbleTex = new THREE.TextureLoader().load("textures/cobble.jpg");
+            cobbleTex.repeat.set(6,6);
+            cobbleTex.wrapS = THREE.RepeatWrapping;
+            cobbleTex.wrapT = THREE.RepeatWrapping;
+            const wallGroup = new THREE.Group();
+            var map = [ // 1  1  3  4  5  6  7  8  9
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1,], // 0
+                [1, 0, 0, 0, 0, 0, 0, 0, 0, 1,], // 1
+                [1, 0, 1, 1, 1, 1, 1, 1, 0, 1,], // 1
+                [1, 0, 1, 0, 0, 0, 0, 1, 0, 1,], // 3
+                [1, 0, 1, 0, 1, 1, 0, 1, 0, 1,], // 4
+                [1, 0, 1, 0, 1, 1, 0, 1, 0, 1,], // 5
+                [1, 0, 1, 0, 0, 0, 0, 1, 0, 1,], // 6
+                [1, 0, 1, 1, 1, 1, 1, 1, 0, 1,], // 7
+                [1, 0, 0, 0, 0, 0, 0, 0, 0, 1,], // 8
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1,], // 9
+            ], mapW = map.length, mapH = map[0].length;
+            for(var row = 0; row < mapW; row++){
+                for(var col = 0; col < mapH; col++)
+                    if(map[row][col] == 1){
+                        const wallGeo = new THREE.BoxBufferGeometry(WALL_WIDTH, WALL_HEIGHT, WALL_DEPTH, 0);
+                        const wallMat= new THREE.MeshPhongMaterial({map:  cobbleTex});
+                        const wall = new THREE.Mesh(wallGeo, wallMat);
+                        wallGroup.add(wall);
+                        wall.translateX(row*WALL_WIDTH);
+                        wall.translateY(col*WALL_WIDTH);
+                    }
+            }
+            return wallGroup;
+        }
+    }
+    return Walls;
+});
