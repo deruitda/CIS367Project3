b719b9057cf75baefbb1f5c36fb7f687f628e858
Danny DeRuiter <dannydf>
2017-04-17 17:06:25 -0400

    fuck my life

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+var scene, camera, renderer, projector;
+var geometry, material, mesh;
+var wheelOne, wheelCF, tmpTranslation, tmpRotation, tmpScale;
+var target, TargetCF;
+var testWall, wallCF;
+var clock, controls;
+var bullets;
+var targets;
+var WIDTH = window.innerWidth,
+    HEIGHT = window.innerHeight,
+    ASPECT = WIDTH / HEIGHT,
+    UNITSIZE = 200,
+    WALLHEIGHT = UNITSIZE / 3,
+    MOVESPEED = 300,
+    LOOKSPEED = 0.075,
+    BULLETMOVESPEED = MOVESPEED * 5,
+    NUMAI = 5,
+    PROJECTILEDAMAGE = 20,
+    map;
+var mouse = { x: 0, y: 0 };
+var gun;
 require({
     // baseUrl: 'js',
     // three.js should have UMD support soon, but it currently does not
@@ -6,31 +27,9 @@ require({
     //     '/bower_components/threex.windowresize/threex.windowresize': { exports: 'THREEx' }
     // }
 }, [
-    'vendor/three', 'models/Wheel', 'models/Target', 'models/Walls', 'vendor/FirstPersonControls'
-], function(THREE, Wheel, Target, Walls, FPSControls) {
+    'vendor/three', 'models/Target', 'models/Walls', 'vendor/FirstPersonControls'
+], function(THREE, Target, Walls, FPSControls) {
 
-    var scene, camera, renderer, projector;
-    var geometry, material, mesh;
-    var wheelOne, wheelCF, tmpTranslation, tmpRotation, tmpScale;
-    var target, TargetCF;
-    var testWall, wallCF;
-    var clock, controls;
-    var bullets;
-    var targets;
-    var WIDTH = window.innerWidth,
-        HEIGHT = window.innerHeight,
-        ASPECT = WIDTH / HEIGHT,
-        UNITSIZE = 250,
-        WALLHEIGHT = UNITSIZE / 3,
-        MOVESPEED = 300,
-        LOOKSPEED = 0.075,
-        BULLETMOVESPEED = MOVESPEED * 5,
-        NUMAI = 5,
-        PROJECTILEDAMAGE = 20;
-    var mouse = { x: 0, y: 0 };
-
-
-    const rotZ1 = new THREE.Matrix4().makeRotationZ(THREE.Math.degToRad(1));
 
     init();
     animate();
@@ -41,6 +40,7 @@ require({
         scene = new THREE.Scene();
         clock = new THREE.Clock();
         bullets  = [];
+        scene.background = new THREE.Color(0x42adf4);
         window.addEventListener('resize', onResize, false);
         document.addEventListener( 'mousemove', onDocumentMouseMove, false );
         //window.addEventListener('keydown', onKeypress, false);
@@ -55,16 +55,16 @@ require({
         controls = new FPSControls(camera);
         controls.movementSpeed = MOVESPEED;
         controls.lookSpeed = LOOKSPEED;
-        //controls.lookVertical = false; // Temporary solution; play on flat surfaces only
+        controls.lookVertical = false; // Temporary solution; play on flat surfaces only
         controls.noFly = false;
 
-        const lightOne = new THREE.DirectionalLight(0xFFFFFF, 1.2);
-        lightOne.position.set(100, 40, 400);
+        var loader = new THREE.JSONLoader();
+        loader.load( "textures/desert_eagle.json", addModelToScene );
+
+        const lightOne = new THREE.AmbientLight(0xFFFFFF);
+        lightOne.position.set(0, 100, 0);
         scene.add(lightOne);
 
-        const lightTwo = new THREE.DirectionalLight(0xFFFFFF, 1.2);
-        lightTwo.position.set(300, 80, 400);
-        scene.add(lightTwo);
         // const lightOneHelper = new THREE.DirectionalLightHelper(lightOne, 100);
         // scene.add(lightOneHelper);
         geometry = new THREE.BoxGeometry( 200, 200, 200 );
@@ -88,29 +88,26 @@ require({
         tmpRotation = new THREE.Quaternion();
         tmpTranslation = new THREE.Vector3();
         tmpScale = new THREE.Vector3();
-        wheelOne = new Wheel();
-        const targetGeo = new THREE.CylinderGeometry(50, 50, 10, 50);
-        const targetMat = new THREE.MeshPhongMaterial ({color: 0xff0000});
-        target = new THREE.Mesh(targetGeo, targetMat);
-        targets = [];
-        targets.push(target);
-
-        scene.add(target);
 
         testWall = new Walls();
+        testWall.translateY(100);
         //scene.add(testWall);
+        map = testWall.map;
         testWall.rotateX(-(Math.PI/2));
+
+        targets = [];
+        createTargets();
 
         var container = document.getElementById("container");
         renderer = new THREE.WebGLRenderer();
         renderer.setSize( window.innerWidth, window.innerHeight );
 
-        const gravelTex = new THREE.TextureLoader().load("textures/gravel.png");
+        const gravelTex = new THREE.TextureLoader().load("textures/cobble2.png");
         gravelTex.repeat.set(6,6);
         gravelTex.wrapS = THREE.RepeatWrapping;
         gravelTex.wrapT = THREE.RepeatWrapping;
         const ground = new THREE.Mesh (
-            new THREE.PlaneGeometry(5500, 5500, 10, 10),
+            new THREE.PlaneGeometry(10000, 10000, 10, 10),
             new THREE.MeshPhongMaterial({ map: gravelTex})
         );
         scene.add(ground);
@@ -123,6 +120,13 @@ require({
             }
         });
         document.body.appendChild( renderer.domElement );
+    }
+
+    function addModelToScene(geometry, materials){
+        var material = new THREE.MeshFaceMaterial(materials);
+        gun = new THREE.Mesh( geometry, material );
+        gun.position.set(0,10,0);
+        scene.add( model );
     }
 
     function animate() {
@@ -140,36 +144,36 @@ require({
         renderer.setSize (width, height);
     }
 
-    var sphereMaterial = new THREE.MeshBasicMaterial({color: 0xffff00});
-    var sphereGeo = new THREE.SphereGeometry(1, 1, 1);
+    var lineMaterial = new THREE.MeshBasicMaterial({color: 0xffff00});
+    var lineGeo = new THREE.BoxGeometry(3, 3, 3, 3, 3, 3);
     function createBullet(obj) {
         if (obj === undefined) {
             obj = camera;
         }
-        var sphere = new THREE.Mesh(sphereGeo, sphereMaterial);
-        sphere.position.set(obj.position.x, obj.position.y * 0.8, obj.position.z);
+        var line = new THREE.Mesh(lineGeo, lineMaterial);
+        line.position.set(obj.position.x, obj.position.y * 0.8, obj.position.z);
 
         if (obj instanceof THREE.Camera) {
             var vector = new THREE.Vector3(mouse.x, mouse.y, 1);
             projector.unprojectVector(vector, obj);
-            sphere.ray = new THREE.Ray(
+            line.ray = new THREE.Ray(
                 obj.position,
                 vector.subSelf(obj.position).normalize()
             );
         }
         else {
             var vector = camera.position.clone();
-            sphere.ray = new THREE.Ray(
+            line.ray = new THREE.Ray(
                 obj.position,
                 vector.subSelf(obj.position).normalize()
             );
         }
-        sphere.owner = obj;
+        line.owner = obj;
 
-        bullets.push(sphere);
-        scene.add(sphere);
+        bullets.push(line);
+        scene.add(line);
 
-        return sphere;
+        return line;
     }
 
     function onDocumentMouseMove(e) {
@@ -179,6 +183,21 @@ require({
     }
 
 
+    function createTargets(){
+        var numTargets = 20;
+        for(var row = 0; row < map.length; row++){
+
+        }
+        for(var i = 0; i < numTargets; i++){
+            const targetGeo = new THREE.CylinderGeometry(50, 50, 10, 50);
+            const targetMat = new THREE.MeshPhongMaterial ({color: 0xff0000});
+            target = new THREE.Mesh(targetGeo, targetMat);
+
+            target.rotateX(Math.PI/2);
+            targets.push(target);
+        }
+    }
+
     // Update and display
     function render() {
         var delta = clock.getDelta(), speed = delta * BULLETMOVESPEED;
@@ -187,35 +206,52 @@ require({
         // Update bullets. Walk backwards through the list so we can remove items.
         if(bullets.length != undefined){
             for (var i = bullets.length-1; i >= 0; i--) {
-                var b = bullets[i], bulletPos = b.position, bulletDir = b.ray.direction;
-                //if()
-
+                var bullet = bullets[i];
+                var dir = bullet.ray.direction;
                 var hit = false;
-                //for (var j = ai.length-1; j >= 0; j--) {
-                if(targets[0]) {
-                    var target = targets[0];
-                    var ray = new THREE.Raycaster(bulletPos, bulletDir.clone().normalize());
-                    var colResults = ray.intersectObject(target);
-                    if(colResults.length > 0 && colResults[0].distance < bulletDir.length){
-                        scene.remove(b);
-                        hit = true;
-                    }
-                    if (hit) {
-                        scene.remove(target);
-                        window.alert("FUCK!!!");
-                    }
-                    //}
-                    if (!hit) {
-                        b.translateX(speed * bulletDir.x);
-                        b.translateZ(speed * bulletDir.z);
-                        b.translateY(speed * bulletDir.y);
-                    }
+                var originPoint = bullet.position.clone();
+                if(checkWallCollision(bullet.position)){
+                    bullets.splice(i, 1);
+                    scene.remove(bullet);
+                    continue;
                 }
+                for(var vertexIndex = 0; vertexIndex < bullet.geometry.vertices.length; vertexIndex++){
+                    var localVertex = bullet.geometry.vertices[vertexIndex].clone();
+                    var globalVertex = localVertex.applyMatrix4(bullet.matrix);
+                    var directionVector = globalVertex.sub(bullet.position);
+
+                    var ray = new THREE.Raycaster(originPoint, directionVector.clone().normalize());
+                    var collisionResults = ray.intersectObjects(targets);
+                    if(collisionResults.length > 0 && collisionResults[0].distance < directionVector.length)
+                        hit = true;
+                }
+
+                if (hit) {
+                   scene.remove(target);
+                   window.alert("FUCK!!!");
+                }
+                if (!hit) {
+                    bullet.translateX(speed * dir.x);
+                    bullet.translateZ(speed * dir.z);
+                    bullet.translateY(speed * dir.y);
+                }
+
             }
         }
 
         renderer.render(scene, camera); // Repaint
     }
 
+    function getMapSector(v) {
+        var x = Math.floor((v.x + UNITSIZE / 2) / UNITSIZE + map.length/2);
+        var z = Math.floor((v.z + UNITSIZE / 2) / UNITSIZE + map.length/2);
+        return {x: x, z: z};
+    }
+
+    function checkWallCollision(v) {
+        var c = getMapSector(v);
+        //return map[c.x][c.z] > 0;
+        return 0;
+    }
 
 });
--- js/models/Walls.js
+++ js/models/Walls.js
@@ -5,38 +5,32 @@ define(function(require){
     var THREE = require('../vendor/three');
     class Walls{
         constructor(){
-            const WALL_HEIGHT = 100;
-            const WALL_WIDTH = 100;
-            const WALL_DEPTH = 1000;
-            const cobbleTex = new THREE.TextureLoader().load("textures/cobble.jpg");
+            const WALL_HEIGHT = 500;
+            const WALL_WIDTH = 500;
+            const WALL_DEPTH = 200;
+            const cobbleTex = new THREE.TextureLoader().load("textures/brick.jpg");
             cobbleTex.repeat.set(6,6);
             cobbleTex.wrapS = THREE.RepeatWrapping;
             cobbleTex.wrapT = THREE.RepeatWrapping;
             const wallGroup = new THREE.Group();
-            var map = [ // 1  1  3  4  5  6  7  8  9
-                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // 0
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], // 1
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], // 1
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], // 3
-                [1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], // 4
-                [1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], // 5
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], // 6
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], // 7
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], // 7
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], // 7
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], // 7
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], // 7
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],// 8
-                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // 9
-            ], mapW = map.length, mapH = map[0].length;
+            wallGroup.map = [ // 1  1  3  4  5  6  7  8  9
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // 0
+                [1, 0, 0, 0, 0, 0, 0, 0, 0, 1], // 1
+                [1, 0, 1, 1, 1, 1, 1, 1, 0, 1], // 1
+                [1, 0, 1, 0, 0, 0, 0, 1, 0, 1], // 3
+                [1, 0, 1, 0, 1, 1, 0, 1, 0, 1], // 4
+                [1, 0, 1, 0, 1, 1, 0, 1, 0, 1], // 5
+                [1, 0, 1, 0, 0, 0, 0, 1, 0, 1], // 6
+                [1, 0, 1, 1, 1, 1, 1, 1, 0, 1], // 7
+                [1, 0, 0, 0, 0, 0, 0, 0, 0, 1], // 8
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1] // 9
+
+            ];
+            var mapW = wallGroup.map.length, mapH = wallGroup.map[0].length;
             for(var row = 0; row < mapW; row++){
                 for(var col = 0; col < mapH; col++)
-                    if(map[row][col] == 1){
-                        const wallGeo = new THREE.BoxBufferGeometry(WALL_WIDTH, WALL_HEIGHT, WALL_DEPTH, 0);
+                    if(wallGroup.map[row][col] == 1){
+                        const wallGeo = new THREE.BoxBufferGeometry(WALL_WIDTH, WALL_HEIGHT, WALL_DEPTH, 1, 1, 1);
                         const wallMat= new THREE.MeshPhongMaterial({map:  cobbleTex});
                         const wall = new THREE.Mesh(wallGeo, wallMat);
                         wallGroup.add(wall);
